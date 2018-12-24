98fab7ecf8f1f97e756695920889dfc8c73b708d
FHamster <gaoxin459716010@163.com>
2018-12-24 20:32:41 +0800

    2

--- src/sample/GamePane.java
+++ src/sample/GamePane.java
@@ -24,12 +24,12 @@ public class GamePane extends Pane {
 
 //        Ball ball = new Ball(100, 600, 50, Color.FIREBRICK);
         Ball ball2 = new Ball(30, 730, 20, Color.CORNFLOWERBLUE);
-        Ball ball3 = new Ball(300, 70, 20, Color.CORNFLOWERBLUE);
-        Ball ball4 = new Ball(30, 700, 20, Color.CORNFLOWERBLUE);
+//        Ball ball3 = new Ball(300, 70, 20, Color.CORNFLOWERBLUE);
+//        Ball ball4 = new Ball(30, 700, 20, Color.CORNFLOWERBLUE);
 //        BallController ballController = new BallController(ball, this);
         BallController ballController2 = new BallController(ball2, this);
-        BallController ballController3 = new BallController(ball3, this);
-        BallController ballController4 = new BallController(ball4, this);
+//        BallController ballController3 = new BallController(ball3, this);
+//        BallController ballController4 = new BallController(ball4, this);
 
         Brick brick = Brick.getRandStdBrick();
         BrickController brickController = new BrickController(Brick.getRandStdBrick(), this, 1);
@@ -40,8 +40,8 @@ public class GamePane extends Pane {
 
 //        getChildren().add(ballController.getShape());
         getChildren().add(ballController2.getShape());
-        getChildren().add(ballController3.getShape());
-        getChildren().add(ballController4.getShape());
+//        getChildren().add(ballController3.getShape());
+//        getChildren().add(ballController4.getShape());
         getChildren().add(brickController.getShape());
         getChildren().add(conBrickController.getShape());
 
@@ -61,14 +61,14 @@ public class GamePane extends Pane {
         Thread t2 = new Thread(ballController2);
         Thread t3 = new Thread(brickController);
         Thread t4 = new Thread(conBrickController);
-        Thread t5 = new Thread(ballController3);
-        Thread t6 = new Thread(ballController4);
+//        Thread t5 = new Thread(ballController3);
+//        Thread t6 = new Thread(ballController4);
 //        t.start();
         t2.start();
         t3.start();
         t4.start();
-        t5.start();
-        t6.start();
+//        t5.start();
+//        t6.start();
 
     }
 
